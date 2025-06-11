a0a6473a4943808bb8e4f5f6e4dd61cd58b96688
Your Name <pasapera279@gmail.com>
2025-06-11 16:02:05 -0500

    config

---
+++ server.js
@@ -0,0 +1,17 @@
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// require("dotenv").config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+
+// connectDB();
+
+// app.listen(PORT, () => {
+//     console.log(`✅ Web server running at port: ${PORT}`);
+// });
